10e345801ae0f347226081b58158a75fb84482a1
Kapya Sakala <kapsakala@gmail.com>
2023-01-24 14:57:36 +0200

    addicionei commentarios

--- listBucketItems.js
+++ listBucketItems.js
@@ -1,4 +1,7 @@
-const room_webhook_url = '';
+/*Cloud function that lists all items in a GCS bucket
+Then posts the names of the items to an incoming webhook of a chat space*/
+
+const spaceWebhookURL = '';
 const bucketName = '';
 
 
@@ -6,14 +9,17 @@ const {Storage} = require('@google-cloud/storage');
 const axios = require('axios');
 
 exports.listBucketItems = async (req, res) => {
-
+    /*Connect to GCS*/
     const storage = new Storage();
+    /*Get all files in the bucket supplied*/
     const [files] = await storage.bucket(bucketName).getFiles();
 
-    const fileNamesArr = files.map(file => file.name).join('\n');
+    /*Convert the filenames to a string separated by newline characters*/
+    const fileNamesText = files.map(file => file.name).join('\n');
 
     try {
-        const response = await axios.post(room_webhook_url, {
+        /*Post the filenames string to the space webhook. The param expected is "text"*/
+        const response = await axios.post(spaceWebhookURL, {
             text: fileNamesText
         });
         console.log(response.data);
