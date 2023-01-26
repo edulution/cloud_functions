44c5ea7356344b7deadf679e7107f9b502e601fb
Kapya Sakala <kapsakala@gmail.com>
2023-01-26 14:07:12 +0200

    include created time and last modified time from metadata

--- check_report_exists.js
+++ check_report_exists.js
@@ -1,39 +1,49 @@
 const { Storage } = require('@google-cloud/storage');
 const bucketName = '';
 
+
 exports.checkFile = (req, res) => {
     if (!(req.method === 'POST' && req.body)) {
-          res.status(400).send('')
-      }
+        res.status(400).send('');
+    }
     const event = req.body;
-      let reply = {};
-      if (event.type === 'MESSAGE') {
-        reply = {
+    if (event.type === 'MESSAGE') {
+        /*reply = {
             text: `Hello ${event.user.displayName}`
-        };
-      }
-    const storage = new Storage();
+        };*/
+        // Get the file name from the request
+        const fileName = req.body.message;
 
-    // Get the file name from the request
-    const fileName = req.body.file_name;
+        /*Create a new Storage client*/
+        const storage = new Storage();
 
-    // Connect to the storage bucket
-    const reportsBucket = storage.bucket(bucketName);
+        // Connect to the storage bucket
+        const reportsBucket = storage.bucket(bucketName);
 
-    // Check if the file exists in the bucket
-    reportsBucket.file(fileName).exists()
-        .then(data => {
-            const fileExists = data[0];
-            if (fileExists) {
-                // Notify the user that the file exists
-                res.status(200).send({ message: 'File exists.' });
-            } else {
-                // Notify the user that the file does not exist
-                res.status(404).send({ message: 'File does not exist.' });
-            }
-        })
-        .catch(err => {
-            console.error(err);
-            res.status(500).send({ message: 'An error occurred.' });
-        });
+        // Check if the file exists in the bucket
+        reportsBucket.file(fileName).exists()
+            .then(data => {
+                const fileExists = data[0];
+                if (fileExists) {
+                    // Get the file metadata
+                    file.getMetadata()
+                        .then(metadata => {
+                            const createdTime = metadata[0].timeCreated;
+                            const modifiedTime = metadata[0].updated;
+                            res.send({ text: `File exists. \n Created at ${createdTime}. \n Last modified at ${modifiedTime}` });
+                        })
+                        .catch(err => {
+                            console.error(err);
+                            res.send({ text: 'An error occurred while retrieving file metadata.' });
+                        });
+                } else {
+                    // Notify the user that the file does not exist
+                    res.send({ text: 'File does not exist.' });
+                }
+            })
+            .catch(err => {
+                console.error(err);
+                res.send({ text: 'An error occurred while checking file existence.' });
+            });
+    }
 };
