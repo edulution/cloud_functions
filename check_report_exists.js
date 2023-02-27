509edcc5b99420d500c150d93cbe572fcede1782
Kapya Sakala <kapsakala@gmail.com>
2023-02-27 20:46:03 +0200

    vê se um arquivo do activity ou baseline existe, e diga ao usurario o tamanho e a data de criação do arquivo

--- check_report_exists.js
+++ check_report_exists.js
@@ -3,114 +3,115 @@ const bucketName = '';
 
 
 exports.checkFile = (req, res) => {
-    /*Check the request method. If it is a post, return 400 status and an empty message*/
-    if (!(req.method === 'POST' && req.body)) {
-        res.status(400).send('');
-    }
-    /*Store the request body in the event variable*/
-    const event = req.body;
-    /*handle message events*/
-    if (event.type === 'MESSAGE') {
-        /*Store the message text in a variable*/
-        const messageText = event.message.text;
-        /*Log the message the user sent*/
-        console.log(`user: ${event.user.displayName} sent message ${messageText}`);
+        /*Check the request method. If it is a post, return 400 status and an empty message*/
+        if (!(req.method === 'POST' && req.body)) {
+            res.status(400).send('');
+        }
+        /*Store the request body in the event variable*/
+        const event = req.body;
+        /*handle message events*/
+        if (event.type === 'MESSAGE') {
+            /*Store the message text in a variable*/
+            const messageText = event.message.text;
+            /*Log the message the user sent*/
+            console.log(`user: ${event.user.displayName} sent message ${messageText}`);
 
-        /*Respond to hi, hello, and messages less than 3 characters*/
-        if (messageText == "Hello" || messageText == "Hi" || messageText.length < 3) {
-            res.send({ text: `Hello ${event.user.displayName}. \n Please message me the 3-letter acronymn of a centre and I'll let you know if we've recieved reports from there` });
-        } else {
-            // Get the file name from the message text
-            const fileName = messageText;
-            console.log(`filename ${fileName}`);
+            /*Respond to hi, hello, and messages less than 3 characters*/
+            if (messageText == "Hello" || messageText == "Hi" || messageText.length < 3) {
+                res.send({ text: `Hello ${event.user.displayName}. \n Please message me the 3-letter acronymn of a centre and I'll let you know if we've recieved reports from there` });
+            } else {
+                // Get the file name from the message text
+                const fileName = messageText;
+                console.log(`filename ${fileName}`);
 
-            /*Create a new Storage client*/
-            const storage = new Storage();
+                /*Create a new Storage client*/
+                const storage = new Storage();
 
-            // Connect to the storage bucket
-            const reportsBucket = storage.bucket(bucketName);
-            console.log(`bucketname: ${bucketName}`);
+                // Connect to the storage bucket
+                const reportsBucket = storage.bucket(bucketName);
+                console.log(`bucketname: ${bucketName}`);
 
-            /*Convert the filenames to a string separated by newline characters*/
-            const fileNames = reportsBucket.getFiles().map(file => file.name);
+                /*Convert the filenames to a string separated by newline characters*/
+                const fileNames = reportsBucket.getFiles().map(file => file.name);
 
-            const checkFile = get_most_recent_file(fileNames, fileName);
+                const activityFile = get_most_recent_file(fileNames, "activity", fileName);
+                const baselineFile = get_most_recent_file(fileNames, "baseline", fileName);
 
-            if (checkFile) {
-                reportsBucket.file(checkFile).getMetadata()
-                    .then(metadata => {
-                        const createdTime = metadata[0].timeCreated;
-                        const modifiedTime = metadata[0].updated;
-                        res.send({ text: `Report for centre ${fileName} exists. \n Created at ${createdTime}. \n Last modified at ${modifiedTime}` });
-                    })
-                    .catch(err => {
+
+                if (activityFile && baselineFile) {
+                    const activityFileObj = reportsBucket.file(activityFile);
+                    const baselineFileObj = reportsBucket.file(baselineFile);
+
+                    Promise.all([
+                        activityFileObj.getMetadata(),
+                        baselineFileObj.getMetadata()
+                    ]).then(([activityMetadata, baselineMetadata]) => {
+                        const activitySize = activityMetadata[0].size;
+                        const activityCreatedTime = activityMetadata[0].timeCreated;
+
+                        const baselineSize = baselineMetadata[0].size;
+                        const baselineCreatedTime = baselineMetadata[0].timeCreated;
+                        res.send({
+                            text: `Report for centre *${fileName}* exists. \n Activity File: \nsize=${activitySize}\ncreated at ${activityCreatedTime}\n\n Baseline File:\n size=${baselineSize}\ncreated at ${baselineCreatedTime}`
+                        });
+                    }).catch(err => {
                         console.error(err);
                         res.send({ text: 'An error occurred while retrieving file metadata.' });
                     });
+                } else if (activityFile) {
+                    reportsBucket.file(activityFile).getMetadata()
+                        .then(metadata => {
+                            const createdTime = metadata[0].timeCreated;
+                            const modifiedTime = metadata[0].updated;
 
-            } else {
-                res.send({ text: 'File does not exist.' });
-            }
-
-            // Check if the file exists in the bucket
-            /*reportsBucket.file(checkFile).exists()
-                .then(data => {
-                    const fileExists = data[0];
-                    if (fileExists) {
-                        // Get the file metadata
-                        reportsBucket.file(fileName).getMetadata()
-                            .then(metadata => {
-                                const createdTime = metadata[0].timeCreated;
-                                const modifiedTime = metadata[0].updated;
-                                res.send({ text: `File exists. \n Created at ${createdTime}. \n Last modified at ${modifiedTime}` });
-                            })
-                            .catch(err => {
-                                console.error(err);
-                                res.send({ text: 'An error occurred while retrieving file metadata.' });
+                            res.send({
+                                text: `Report for centre ${fileName} exists. \n Activity File: size=${metadata[0].size}, created at ${createdTime}, modified at ${modifiedTime}. \n No baseline file found.`
                             });
-                    } else {
-                        // Notify the user that the file does not exist
-                        res.send({ text: 'File does not exist.' });
-                    }
-                })
-                .catch(err => {
-                    console.error(err);
-                    res.send({ text: 'An error occurred while checking file existence.' });
-                });*/
-        }
-    } else {
-        /*Log the event type*/
-        console.log(`user: ${event.user.displayName} sent event of type ${event.type}`);
-        /*If the event is not a message, ask the user to send the expected kind of message*/
-        res.send({ text: `Hello ${event.user.displayName}. \n Please message me the 3-letter acronymn of a centre and I'll let you know if we've recieved reports from there` });
-    }
-};
+                        })
+                        .catch(err => {
+                            console.error(err);
+                            res.send({ text: 'An error occurred while retrieving file metadata.' });
+                        });
+                } else if (baselineFile) {
+                    reportsBucket.file(baselineFile).getMetadata()
+                        .then(metadata => {
+                            const createdTime = metadata[0].timeCreated;
+                            const modifiedTime = metadata[0].updated;
+                            res.send({
+                                text: `Report for centre *${fileName}* exists. No activity file found.\n\n *Baseline File*:\n size=${metadata[0].size}\n created at ${createdTime}`
+                            });
+                        })
+                        .catch(err => {
+                            console.error(err);
+                            res.send({ text: 'An error occurred while retrieving file metadata.' });
+                        });
+
+                } else {
+                    /*Log the event type*/
+                    console.log(`user: ${event.user.displayName} sent event of type ${event.type}`);
+                    /*If the event is not a message, ask the user to send the expected kind of message*/
+                    res.send({ text: `Hello ${event.user.displayName}. \n Please message me the 3-letter acronymn of a centre and I'll let you know if we've recieved reports from there` });
+                }
+            };
 
 
 /*Function to get most recent file from a list of filenames*/
-function get_most_recent_file(filenames, inputString) {
-    // Initialize variables to store the most recent filename and date
-    let mostRecentFilename = null;
-    let mostRecentDate = null;
+function get_most_recent_file(filenames, report_type, centre_id) {
+  let mostRecentFilename = null;
+  let mostRecentDateTime = null;
 
-    // Iterate through each filename in the filenames array
-    for (let i = 0; i < filenames.length; i++) {
-        // Split the filename using underscores
-        const splitFilename = filenames[i].split("_");
-        // Get the file prefix
-        const filePrefix = splitFilename[1];
-        // Get the file date
-        const fileDate = new Date(`${splitFilename[3]}-01`);
+  const regex = new RegExp(`^${report_type}_${centre_id}_[0-9]{2}-[0-9]{2}_(\\d{14})\\..*$`);
 
-        // Compare filePrefix to the input string
-        if (filePrefix === inputString) {
-            // If filePrefix matches the input string, check if the file date is more recent than the current most recent date
-            if (mostRecentDate === null || fileDate > mostRecentDate) {
-                mostRecentFilename = filenames[i];
-                mostRecentDate = fileDate;
-            }
-        }
+  for (let i = 0; i < filenames.length; i++) {
+    const match = filenames[i].match(regex);
+    if (match) {
+      const dateTime = match[1];
+      if (mostRecentDateTime === null || dateTime > mostRecentDateTime) {
+        mostRecentFilename = filenames[i];
+        mostRecentDateTime = dateTime;
+      }
     }
-    // Return the most recent filename or "Not found" if no matching file prefix was found
-    return mostRecentFilename ? mostRecentFilename : null;
+  }
+
+  return mostRecentFilename || "No file found";
 }
